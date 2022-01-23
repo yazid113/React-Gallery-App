29ad7f6a064ae5394d59178744c614439b1d7aac
yazid113 <92461753+yazid113@users.noreply.github.com>
2022-01-23 12:06:17 -0500

    Update

--- react-gallery-app/src/App.js
+++ react-gallery-app/src/App.js
@@ -1,10 +1,13 @@
 import Photo from "./Components/Photo";
-
+import SearchBar from "./Components/SearchBar";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Photo/>
+      <SearchBar />
+
+      <NavBar />
     </div>
   );
 }
